2efe0a8247db31a0d37cfbbb898ca93dcd24caf2
Mcdiek <diegogantiva@hotmail.com>
2021-10-11 15:26:04 -0500

    conexion baase de datos productos

--- page-one/src/components/ModuloProductos.js
+++ page-one/src/components/ModuloProductos.js
@@ -28,25 +28,28 @@ export const ModuloProductos = () => {
         obtenerDatos()
     }, [])
 
-    const capuraDatos = (e) => {
-        e.preventDefault()
+    const obtenerDatos = async () => {
+        const datos = await getDocs(collection(db, 'productos'));
+        let envioDatos = [];
+        datos.forEach((documento) => {
+            envioDatos.push(documento.data())
+        })
+        setListaProductos(envioDatos)
+    }
 
+    const capuraDatos = (e) => {
+        e.preventDefault()        
         setNombreProducto(e.target.id === 'inputName' ? e.target.value : nombreProducto)
         setDescripcion(e.target.id === 'descripcion' ? e.target.value : descripcion)
         setValorUnitario(e.target.id === 'valorUnitario' ? e.target.value : valorUnitario)
-        setEstado(e.target.id === 'estado' ? e.target.value : estado)   
-          
-
+        setEstado(e.target.id === 'estado' ? e.target.value : estado) 
+        setId(!editar ? nanoid() : id)                  
     }
+  
 
     const handleFormulario = (e) => {
         e.preventDefault()
-        setId(nanoid())  
-        sendData(); 
-
-        // Prueba del nano id
-        // console.log('ID: ',nanoid(10));
-
+        
         if (!nombreProducto.trim()) {
             console.log('Debes ingresar un nombre');
             return
@@ -65,34 +68,19 @@ export const ModuloProductos = () => {
         if (!estado.trim()) {
             console.log('Debes ingresar estado');
             return
-        }
-
-       
-        setListaProductos([
-            ...listaProductos,
-            {
-                id,
-                // tarea: 'valor de la veriable'
-                // tarea: tarea
-                nombreProducto,
-                descripcion,
-                valorUnitario,
-                estado
-            }
-        ])
-
-              
+        }  
         
+        sendData()
 
         setNombreProducto('')
         setDescripcion('')
         setValorUnitario('')
         setEstado('Seleccione')   
         setId('')  
-
+      
     }
 
-    const sendData = async()=>{        
+    const sendData = async()=>{          
       await setDoc(doc(db,"productos",id),
        {
         id: id,
@@ -101,6 +89,8 @@ export const ModuloProductos = () => {
         valorUnitario: valorUnitario,
         estado: estado
       })
+
+      obtenerDatos()
     }
 
     const deleteData = async(id)=>{        
@@ -131,7 +121,7 @@ export const ModuloProductos = () => {
 
         sendData()
 
-        const arregloTemporal = listaProductos.map((item) => {
+        const arregloTemporal = listaProductos.map((item) => {            
             return item.id === id ? {
                 id: id,
                 nombreProducto: nombreProducto,
@@ -140,6 +130,8 @@ export const ModuloProductos = () => {
                 estado: estado
             } : item
         })
+
+        
 
         setListaProductos(arregloTemporal)
 
@@ -195,7 +187,7 @@ export const ModuloProductos = () => {
                                             className="btn btn-outline-danger btn-sm float-end mx-1"
                                             onClick={() => handleEliminar(product.id)}
                                         >
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                                             </svg>
                                         </button>
@@ -203,7 +195,7 @@ export const ModuloProductos = () => {
                                             className="btn btn-outline-warning btn-sm float-end mx-1"
                                             onClick={() => handleEditar(product)}
                                         >
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-tools" viewBox="0 0 16 16">
                                                 <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.356 3.356a1 1 0 0 0 1.414 0l1.586-1.586a1 1 0 0 0 0-1.414l-3.356-3.356a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0zm9.646 10.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708zM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11z" />
                                             </svg>
                                         </button>
