7d007d1d20fc7ab2f0d2fe22de06b20913c2690c
Mcdiek <diegogantiva@hotmail.com>
2021-10-10 19:11:14 -0500

    first commit

---
+++ page-one/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import shoe from './images/shoes.svg'
+
+export const Login = () => {
+    return (
+        <div>
+            <form className = "form-signin" >
+                <img className ="mb.4" src = {shoe} alt ="zapato"/>
+                <h1 className="h3 mb-3 fw-normal">Ingreso al Sistema de Informacion</h1>
+
+            </form>
+        </div>
+    )
+}
